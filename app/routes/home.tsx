67758b2fbb7d29113ac7b48fb819e9f9dc4b1964
Mohamed Alaa <mohamedalaaelmenyawe606@gmail.com>
2025-12-30 20:51:53 +0200

    update Home component and remove Welcome component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,18 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Applicant Tracking System" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+   <section>
+    <div className="page-heading">
+      <h1>Track Your Application & Resume Rating</h1>
+    </div>
+   </section>
+  </main>
 }
