cbcad9e2ef4e32fc7e55bc251714710b30314aa1
Mohamed Alaa <mohamedalaaelmenyawe606@gmail.com>
2026-01-04 21:32:14 +0200

    Add Navbar component, resume data structure

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants/index";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,10 +12,20 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
    <section className="main-section">
     <div className="page-heading">
       <h1>Track Your Application & Resume Rating</h1>
+      <h2>Review your submissions and check AI-powered feedback.</h2>
     </div>
    </section>
+
+ {resumes.length > 0 && (
+  <div className="resumes-section">
+    {resumes.map((resume:any) => (
+    <ResumeCard key={resume.id} resume={resume} />
+  ))}
+  </div>
+ )}
   </main>
 }
