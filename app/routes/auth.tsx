86fb8a1f3be34b0291e3763c26f0f4a0a97275ab
Mohamed Alaa <mohamedalaaelmenyawe606@gmail.com>
2026-01-05 21:16:05 +0200

    add authentication route; update home meta title

---
+++ app/routes/auth.tsx
@@ -0,0 +1,13 @@
+export function meta() {
+  return [
+    { title: "RESUMIND | Auth" },
+    { name: "description", content: "Smart feedback for your dream job!" },
+  ];
+}
+const auth = () => {
+  return (
+    <div>auth</div>
+  )
+}
+
+export default auth
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,7 +5,7 @@ import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Applicant Tracking System" },
+    { title: "RESUMIND" },
     { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
