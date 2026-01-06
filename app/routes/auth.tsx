9e2fdc340b36b8739884756d9ab43d11df52b3f5
Mohamed Alaa <mohamedalaaelmenyawe606@gmail.com>
2026-01-06 20:55:43 +0200

    Update auth component to include loading state and authentication buttons

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,13 +1,44 @@
+import { usePuterStore } from "~/lib/puter";
+
 export function meta() {
   return [
-    { title: "RESUMIND | Auth" },
-    { name: "description", content: "Smart feedback for your dream job!" },
+    { title: "Resumind | Auth" },
+    { name: "description", content: "Log into your account" },
   ];
 }
 const auth = () => {
-  return (
-    <div>auth</div>
-  )
+  const { isLoading , auth } = usePuterStore()
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+    <div className="gradient-border shadow-lg">
+      <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1>Welcom</h1>
+          <h2>Log In to Continue Your Job Journey</h2>
+        </div>
+        <div>
+          {isLoading ? (
+            <button className="auth-button animate-pulse">
+              <p>Signing you in...</p>
+            </button>
+          ): (
+            <>
+            {auth.isAuthenticated ? (
+              <button className="auth-button" onClick={auth.signOut}>
+                <p>Log Out</p>
+              </button>
+            ) : (
+              <button className="auth-button" onClick={auth.signIn}>
+                <p>Log In</p>
+              </button>
+            )}
+            </>
+          )}
+        </div>
+      </section>
+    </div>
+   
+  </main>
+  
 }
 
 export default auth
