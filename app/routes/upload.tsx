17c2ca26c06ad9cb27e34b93a6cc81995e37098f
Mohamed Alaa <mohamedalaaelmenyawe606@gmail.com>
2026-01-07 13:23:18 +0200

    Add upload route and component for file uploads

---
+++ app/routes/upload.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '~/components/Navbar'
+
+const upload = () => {
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+   <section className="main-section">
+    
+ 
+ </section>
+  </main>
+  )
+}
+
+export default upload
